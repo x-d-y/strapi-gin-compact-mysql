ce5b0ef7bf742f50850af5a1c5875c2ffd2de07e
xdy <xdy@xdy.com>
2019-06-22 19:09:42 +0800

    add mogodb

--- api/template/controllers/handler.go
+++ api/template/controllers/handler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	getRoutes "github.com/xdy/gin/utils/getRoutes"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Routers struct {
@@ -20,65 +21,76 @@ func bindHandler() map[string]reflect.Value {
 	vf := reflect.ValueOf(&ruTest)
 	mNum := vf.NumMethod()
 	vft := vf.Type()
-	fmt.Println("NumMethod:", mNum)
+	//fmt.Println("NumMethod:", mNum)
 	for i := 0; i < mNum; i++ {
 		mName := vft.Method(i).Name
-		fmt.Println("index:", i, " MethodName:", mName)
+		//fmt.Println("index:", i, " MethodName:", mName)
 		crMap[mName] = vf.Method(i)
 	}
 	return crMap
 }
 
-func LoadHandler(router *gin.Engine, routerHandeler map[string][]getRoutes.RouteInfo) {
+var client *mongo.Client
+
+func LoadHandler(router *gin.Engine, routerHandeler map[string][]getRoutes.RouteInfo, client_ *mongo.Client) {
+	client = client_
 	crMap := bindHandler()
-	fmt.Println(crMap)
+	//fmt.Println(crMap)
 	for k, v := range routerHandeler {
 		if k == cPath {
 			groupRoute := router.Group(k)
-			for _, u := range v {
+			for i, u := range v {
 				u_ := u
+				fmt.Println(i)
 				Handler := []rune(u_.Handler)
 				if Handler[0] >= 97 && Handler[0] <= 122 {
 					Handler[0] -= 32
 				}
-				if u.Method == "GET" {
-					groupRoute.GET(u.Path, func(c *gin.Context) {
+				if u_.Method == "GET" {
+					groupRoute.GET(u_.Path, func(c *gin.Context) {
 						parms := []reflect.Value{reflect.ValueOf(c)}
 						crMap[string(Handler)].Call(parms)
 					})
-				} else if u.Method == "POST" {
-					groupRoute.POST(u.Path, func(c *gin.Context) {
+				} else if u_.Method == "POST" {
+					groupRoute.POST(u_.Path, func(c *gin.Context) {
 						parms := []reflect.Value{reflect.ValueOf(c)}
 						crMap[string(Handler)].Call(parms)
 					})
-				} else if u.Method == "PUT" {
-					groupRoute.PUT(u.Path, func(c *gin.Context) {
+				} else if u_.Method == "PUT" {
+					groupRoute.PUT(u_.Path, func(c *gin.Context) {
 						parms := []reflect.Value{reflect.ValueOf(c)}
 						crMap[string(Handler)].Call(parms)
 					})
-				} else if u.Method == "DELETE" {
-					groupRoute.DELETE(u.Path, func(c *gin.Context) {
+				} else if u_.Method == "DELETE" {
+					groupRoute.DELETE(u_.Path, func(c *gin.Context) {
 						parms := []reflect.Value{reflect.ValueOf(c)}
 						crMap[string(Handler)].Call(parms)
 					})
-				} else if u.Method == "PATCH" {
-					groupRoute.PATCH(u.Path, func(c *gin.Context) {
+				} else if u_.Method == "PATCH" {
+					groupRoute.PATCH(u_.Path, func(c *gin.Context) {
 						parms := []reflect.Value{reflect.ValueOf(c)}
 						crMap[string(Handler)].Call(parms)
 					})
 				} else {
+					fmt.Println("unmatch any method")
 					return
 				}
 			}
 		}
-
 	}
 }
 
-func (r *Routers) Test1Hadler(c *gin.Context) {
+/**************************************custom******************************************/
+
+func (r *Routers) InsertOne(c *gin.Context) {
 	fmt.Println("this is test1Handler")
 }
-
-func (r *Routers) Test2Hadler(c *gin.Context) {
+func (r *Routers) UpdateOne(c *gin.Context) {
+	fmt.Println("this is test2Handler")
+}
+func (r *Routers) FindOne(c *gin.Context) {
+	fmt.Println("this is test2Handler")
+}
+func (r *Routers) DeleteOne(c *gin.Context) {
 	fmt.Println("this is test2Handler")
 }
--- config/function/startup.go
+++ config/function/startup.go
@@ -5,12 +5,14 @@ import (
 	template "github.com/xdy/gin/api/template/controllers"
 	test "github.com/xdy/gin/api/test/controllers"
 	getRoutes "github.com/xdy/gin/utils/getRoutes"
+	mongodb "github.com/xdy/gin/utils/mongoDbDriver"
 )
 
 func Startup(router *gin.Engine) map[string][]getRoutes.RouteInfo {
 	route := getRoutes.Routes() //解析路由
 	//route_ := route.(map[string][]interface{})
-	template.LoadHandler(router, route)
-	test.LoadHandler(router, route)
+	client := mongodb.MongodbInitial()
+	template.LoadHandler(router, route, client)
+	test.LoadHandler(router, route, client)
 	return route
 }
